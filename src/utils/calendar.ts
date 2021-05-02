05268545352c5883689039c56967dbc6ecb9791d
Charlie <charlieyang1040@hotmail.com>
2021-05-02 14:26:11 +1200

    Created map function to extract data out of json

--- src/utils/calendar.ts
+++ src/utils/calendar.ts
@@ -1,6 +1,12 @@
- 
-
 //TODO change return type
-export function formatCalendar (icsJSON : any) : any {
-    return icsJSON[2][1][0];
+export function formatCalendar(icsJSON: any): any[] {
+    let events = icsJSON[2].slice(1).map((x: any[]) => {
+        return {
+            name: x[0],
+            startDate: x[1][0][3],
+            endDate: x[1][1][3],
+            description: x[1][5][3],
+        };
+    });
+    return events;
 }
