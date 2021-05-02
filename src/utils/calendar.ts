e35d6645cb6b5dbe9df94303effb578cf4db67db
Charlie <charlieyang1040@hotmail.com>
2021-05-02 22:33:08 +1200

    Implemented basic algorithm

--- src/utils/calendar.ts
+++ src/utils/calendar.ts
@@ -1,10 +1,11 @@
-export interface Event {     
+export interface Event {
     name: String;
-    startDate: Number;
-    endDate: Number;
-} 
+    startDate: number;
+    endDate: number;
+}
 
 export interface Preferences {
+    startTime: number;
     maxInterval?: number;
     blockTimes: Event[];
 }
@@ -25,10 +26,107 @@ export function formatCalendar(icsJSON: any): Event[] {
         let event: Event = {
             name: x[0],
             startDate: new Date(x[1][startDateIndex][3]).getTime(),
-            endDate: new Date(x[1][endDateIndex][3]).getTime(),
+            endDate: new Date(x[1][endDateIndex][3]).getTime()
         };
 
         return event;
     });
     return events;
+}
+
+export function scheduleJobs( events: Event[], jobs: Job[], preferences: Preferences) {
+    let currentTime: number;
+    if (preferences.startTime != null) {
+        currentTime = preferences.startTime;
+    } else {
+        let now = Date.now();
+        currentTime = now - (now % 3600000) + 3600000;
+    }
+    const interval = preferences.maxInterval
+        ? preferences.maxInterval * 3600000
+        : 3600000;
+    let scheduled: Event[] = [];
+    const toSchedule = jobs.map((x) => {
+        return {
+            name: x.name,
+            estimatedTime: x.estimatedTime,
+            deadline: x.deadline,
+            slackTime: x.deadline - currentTime - x.estimatedTime * 3600000
+        };
+    });
+
+    let blockedTimes: Event[] = events.concat(preferences.blockTimes);
+
+    blockedTimes.sort(function (a, b) {
+        return a.endDate - b.endDate;
+    });
+
+    while (!(toSchedule.length === 0)) {
+        blockedTimes = blockedTimes.filter((x) => x.endDate > currentTime);
+        // Start of the iteration
+        toSchedule.sort(function (a, b) {
+            return a.slackTime - b.slackTime;
+        });
+
+        let currentJob = toSchedule[0];
+
+        let plannedStartDate = currentTime;
+        let plannedEndDate = currentTime + interval;
+
+        if (
+            plannedStartDate < blockedTimes[0].startDate &&
+            plannedEndDate < blockedTimes[0].startDate
+        ) {
+            scheduled.push({
+                name: currentJob.name,
+                startDate: plannedStartDate,
+                endDate: plannedEndDate
+            });
+        } else {
+            let index = scheduleBetweenEvents(plannedStartDate, plannedEndDate, blockedTimes);
+            if (index == null){
+                return null;
+            } else {
+                plannedStartDate = blockedTimes[index].startDate
+                plannedEndDate = blockedTimes[index].startDate + interval
+
+                scheduled.push({
+                name: currentJob.name,
+                startDate: plannedStartDate,
+                endDate: plannedEndDate
+            });
+            }
+        }
+
+        currentTime = plannedEndDate
+
+        toSchedule[0].estimatedTime - (interval/3600000)
+
+        toSchedule.forEach( x=> {
+            x.slackTime = x.deadline - currentTime - x.estimatedTime * 3600000
+        })
+        toSchedule.filter(x=> {x.estimatedTime > 0})
+    }
+
+    return scheduled;
+}
+
+function scheduleBetweenEvents(startDate: number, endDate: number, blockedTimes: Event[]
+) {
+    for (let i = 0; i < blockedTimes.length - 1; i++) {
+        if (
+            startDate > blockedTimes[i].endDate &&
+            endDate < blockedTimes[i + 1].startDate
+        ) {
+            return i;
+        }
+    }
+
+    return null;
+}
+
+interface Job {
+    name: String;
+    estimatedTime: number;
+    deadline: number;
 }
