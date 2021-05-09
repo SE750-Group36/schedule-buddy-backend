4e8762c4b4d20a4efa748fceac104f1e8a429106
a-158w <olop920icsroot@gmail.com>
2021-05-09 21:48:14 +1200

    Test added for two events and a single job

--- src/routes/api/__tests__/calendarApi.test.ts
+++ src/routes/api/__tests__/calendarApi.test.ts
@@ -1,5 +1,6 @@
 import { generateSchedule } from "../../../schedule/utils"
 import { oneEventCal } from "../../../examples/oneEventCal";
+import { twoEventCal } from "../../../examples/twoEventCal";
 import { preferenceExample } from "../../../examples/preferenceExample";
 import { oneJob } from "../../../examples/oneJob";
 const {Component} = require('ical.js');
@@ -14,6 +15,21 @@ it("Test scheduler one event and one job", async () => {
 
     const events = schedule.jCal[2].slice(1);
     
-    expect(String(events[0][1][1][3])).toMatch("2021-05-11T12:27:00");
-    expect(String(events[0][1][2][3])).toMatch("2021-05-11T13:27:00");
+    expect(String(events[0][1][1][3])).toMatch("2021-05-11T12:00:00");
+    expect(String(events[0][1][2][3])).toMatch("2021-05-11T13:00:00");
+});
+
+
+
+it("Test scheduler two events and one job", async () => {
+    const jobs = [oneJob];
+    const preferences = preferenceExample;
+    const calendar = twoEventCal;
+
+    let schedule: any = generateSchedule(calendar, jobs, preferences);
+
+    const events = schedule.jCal[2].slice(1);
+    
+    expect(String(events[0][1][1][3])).toMatch("2021-05-11T12:00:00");
+    expect(String(events[0][1][2][3])).toMatch("2021-05-11T13:00:00");
 });
